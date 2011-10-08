6451f26437114ccfaf6d735a2d5a8072f998a61a
Anson Kao <anson.kao@gmail.com>
2011-10-08 06:59:23 -0400

    Updated StudyMonkey 2.0 migration to drop uneeded tables.

--- application/migrations/000_StudyMonkey2.php
+++ application/migrations/000_StudyMonkey2.php
@@ -5,5 +5,19 @@
     ### Make school.full_name indexable for URL re-routing
 	ALTER TABLE `school` ADD INDEX ( `full_name` ) ;
 
+    ### Drop tables we no longer need
+    DROP TABLE `user_password_reset`;
+    DROP TABLE `transaction_paypal`;
+    DROP TABLE `course_enrollment`;
+    DROP TABLE `logging`;
+    DROP TABLE `subscription`;
+    DROP TABLE `transaction_option`;
+    DROP TABLE `transaction_message`;
+    DROP TABLE `note`;
+    DROP TABLE `note_review`;
+    DROP TABLE `program`;
+    DROP TABLE `referral_method`;
+
+
     ";
 
