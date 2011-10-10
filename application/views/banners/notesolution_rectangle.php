3c1de168dbd3c6486176a8b3c0c87ac9632b2a6c
Anson Kao <anson.kao@gmail.com>
2011-10-10 15:55:51 -0400

    Updated notesolution banner ad explanation fine print.

--- application/views/banners/notesolution_rectangle.php
+++ application/views/banners/notesolution_rectangle.php
@@ -6,7 +6,13 @@
     <a href="http://www.notesolution.com" target="_blank">
         <img id="rectangle" src="/image/banners/notesolution_rectangle.jpg" alt="Check out Notesolution.com for Study Notes!" />
     </a>
-    <a href="/notesolution" style="font-size: 11px; text-align: center;">Click here for more information</a>
+    <div style="font-size: 11px; line-height: 13px; color: #555;">
+        Our note-sharing service has moved to Notesolution.com!
+        <br/>
+        <a href="/notesolution" style="text-align: center;">
+            Click here for more info &#187;
+        </a>
+    </div>
 <?php
 
 /* End of file notesolution_rectangle.php */
