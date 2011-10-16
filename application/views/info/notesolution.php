5d714be590b1d86ec9908a29c09ae6afa7bd2043
Anson Kao <anson.kao@gmail.com>
2011-10-16 03:07:14 -0400

    Updated notesolution partnership stuff.

--- application/views/info/notesolution.php
+++ application/views/info/notesolution.php
@@ -3,39 +3,70 @@
  * Notesolution
  */
 ?>
-<p>
-    Over the past few months we've been working hard to bring you an even better StudyMonkey experience.
-    Of course, we will always be continuing to add new features and improve StudyMonkey but for now
-    we are happy to announce that we've launched some recent changes.
-</p>
+<div class="left_column">
 
-<h3>We've partnered with Notesolution.com!</h3>
-<p>
-    We've decided to join forces with an awesome note-sharing website called Notesolution.com to offer you a new and improved note-sharing experience.
-    What does this mean for you? It means that all your notes-related needs will now be moved to Notesolution.com.
-    If you are an existing StudyMonkey user who has shared or bought notes - no need to worry!
-    All of your notes, credits, and earnings will continue to be available on Notesolution.com! Your log-in information is exactly the
-    same as your StudyMonkey information. You just need to follow these instructions:
+    <p>
+        Over the past few months we've been working hard to bring you an even better StudyMonkey experience.
+        Of course, we will always be continuing to add new features and improve StudyMonkey but for now
+        we are happy to announce that we've launched some recent changes.
+    </p>
+
+    <h2>We've partnered with Notesolution.com!</h2>
+    <p>
+        We've decided to join forces with an awesome note-sharing website called Notesolution.com to offer you a new and improved note-sharing experience.
+    </p>
+    <p style="text-align: center;">
+        <img src="/image/press/notesolution_partnership.jpg" alt="Partnership with Notesolution!" />
+    </p>
+
+    <h3>What does this mean for you?</h3>
+    <ul>
+        <li>
+            <p>
+                All your notes-related needs will now be moved to Notesolution.com.  If you have shared or bourght notes - no need to worry!
+                <strong>All of your notes, credits, and earnings will continue to be available on Notesolution.com!</strong>
+            </p>
+        </li>
+        <li>
+            <p>
+                It is now easier to access helpful resources like course reviews and professor ratings on StudyMonkey.
+                <strong>You will no longer need to log in, so it will be easier to find courses and professors, easier to voice your opinion, and easier to share with friends!</strong>
+            </p>
+        </li>
+    </ul>
+
+    <h3>How do you access your notes on Notesolution.com?</h3>
     <ol>
+        <li>Go to Notesolution.com</li>
         <li>XXX</li>
         <li>XXX</li>
     </ol>
-</p>
 
-<h3>More to come!</h3>
-<p>
-    We are always looking for ways to improve your StudyMonkey experience. There are some exciting things we have coming your way that we're
-    sure you'll love. If you have any ideas, suggestions, or feedback, we'd love to hear from you! Feel free to <a href="/contact">contact us</a>.
-</p>
+    <h3>More to come!</h3>
+    <p>
+        We are always looking for ways to improve your StudyMonkey experience. There are some exciting things we have coming your way that we're
+        sure you'll love. If you have any ideas, suggestions, or feedback, we'd love to hear from you! Feel free to <a href="/contact">contact us</a>.
+    </p>
 
-<br/>
+    <br/>
 
-<p>
-    Yours Truly,
-</p>
-<p style="font: italic 14px arial;">
-    The StudyMonkey Team
-</p>
+    <p>
+        Yours Truly,
+    </p>
+    <p style="font: italic 14px arial;">
+        The StudyMonkey Team
+    </p>
+</div>
+
+<!-- ####################################################################### -->
+<div class="right_column">
+    <div style="font-size: 11px; color: #555; padding: 5px 0; text-align: center;">
+        Visit our partner, Notesolution.com
+    </div>
+    <a href="http://www.notesolution.com" target="_blank">
+        <img id="rectangle" src="/image/banners/notesolution_rectangle.jpg" alt="Check out Notesolution.com for Study Notes!" />
+    </a>
+</div>
 <?php
 
 /* End of file notesolution.php */
